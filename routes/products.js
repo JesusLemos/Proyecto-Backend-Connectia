81387f40a8c99855087d7dee690a873bf2c5d7e0
Jesus <JesusLemosFoncubierta@gmail.com>
2019-12-08 14:10:02 +0100

    Añadiendo nuevas rutas de los controladores

--- routes/products.js
+++ routes/products.js
@@ -3,5 +3,5 @@ var router = express.Router();
 let seacher_product_title_controller = require('../controladores/seacher_product_title_controller')
 
 router.get('/productos', seacher_product_title_controller);
-
+router.get('/ventas', salesController);
 module.exports = router;
