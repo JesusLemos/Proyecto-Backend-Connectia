a1838ea2c95a13d8154aab0cef9c7590f33ec5d3
Jesus <JesusLemosFoncubierta@gmail.com>
2019-12-08 14:29:50 +0100

    añadiendo ruta de precio

--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var router = express.Router();
 let seacher_product_title_controller = require('../controladores/seacher_product_title_controller')
+let salesController = require('../controladores/sales_controller')
+let priceController = require('../controladores/price_controller')
 
 router.get('/productos', seacher_product_title_controller);
 router.get('/ventas', salesController);
+router.get('precio', priceController)
+
+
+
 module.exports = router;
