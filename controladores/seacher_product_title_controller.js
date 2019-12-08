b92f6b1e36e72ee6124ec472cf85221e5acf6210
Jesus <JesusLemosFoncubierta@gmail.com>
2019-12-08 12:12:28 +0100

    Añadiendo el endpoint de buscar un producto por titulo

---
+++ controladores/seacher_product_title_controller.js
@@ -0,0 +1,31 @@
+const Sequelize = require('sequelize');
+// const db = require('./models/models.js')
+
+const seacher_product_title_controller = (req, res) => {
+   const db = [{ nombre:'Jesus', edad:20 }, {nombre:'Laura', edad:20}]
+    const seacher = req.body;
+
+    const buscarProducto = db.filter(producto => {
+        producto.nombre === seacher.nombre
+    }
+    )
+    if(buscarProducto){
+        return res
+        .status(201)
+        .send(buscarProducto)
+    }else{
+        return res
+        .status(401)
+        .json({mensaje:'No se encontro el usuario'})
+    }
+
+//     const productos = db.productos;
+//     const encontrarProductos = db.productos.findAll({
+//         attributes: ['title']
+//       }, {where:{
+//          productos.nombre = seacher.nombre
+//       }});
+
+}
+
+module.exports=seacher_product_title_controller;
