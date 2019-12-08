04559145e3153245e98db24d406b83669789b532
Jesus <JesusLemosFoncubierta@gmail.com>
2019-12-08 15:06:37 +0100

    Añadiendo al ruta de visualizar todo

--- routes/products.js
+++ routes/products.js
@@ -3,11 +3,13 @@ var router = express.Router();
 let seacher_product_title_controller = require('../controladores/seacher_product_title_controller')
 let salesController = require('../controladores/sales_controller')
 let priceController = require('../controladores/price_controller')
+let visualizePoduct = require('../controladores/visualize_product_controller')
+
 
 router.get('/productos', seacher_product_title_controller);
 router.get('/ventas', salesController);
 router.get('precio', priceController)
-
+router.get('/visualizarproductos',)
 
 
 module.exports = router;
