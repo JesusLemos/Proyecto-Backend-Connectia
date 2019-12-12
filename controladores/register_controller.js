547e3541fad60bf729e07fb16b46a6be77c2a49f
JesusLemos <JesusLemosFoncubierta@gmail.com>
2019-12-12 18:19:00 +0100

    Retoques al registro

--- controladores/register_controller.js
+++ controladores/register_controller.js
@@ -1,19 +1,44 @@
 const db = require ('../models');
-
+const {contrasenyaConHash,
+    compararContrasenya,
+    crearJWT,
+    decodeJWT} = require('../servicios/autentificacion')
 async function registerController (req, res) {
     try{
+        compararContrasenya(req.body.contrasenya);
+        // req.body.contrasenya = await contrasenyaConHash(req.body.contrasenya)
         const usuario = await db.Usuario.create(req.body);
-        res.status(200).json({ message: 'Registro completado', usuario: usuario})
+        res.status(200).json({ message: 'Registro completado', usuario: usuario});
     }catch (error) {
         console.error(error)
-        res.status(400).json({
+       
+        if (error.message === 'compararContrasenya') {
+            return res.status(400).json({
+              message: 'invalid password',
+              error: error,
+            });
+          }
+          if (error.name === 'SequelizeUniqueConstraintError') {
+            return res.status(400).json({
+              message: 'register invalid',
+              error: error.errors[0].message,
+            });
+          }
+          if (error.name === 'SequelizeValidationError') {
+            return res.status(400).json({
+              message: 'register invalid',
+              error: error.errors[0].message,
+            });
+          }
+          res.status(500).json({
             message: 'register not done',
             error: error,
           });
-        
+        }
+    
     }
 
     
 
-}
+
 module.exports = registerController;
