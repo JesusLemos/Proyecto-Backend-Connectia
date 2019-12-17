3aaa58904fea1cd9f81cd77dcffbccece4812fbc
JesusLemos <JesusLemosFoncubierta@gmail.com>
2019-12-17 18:03:13 +0100

    Añadiendo el endpoint de pedidos

--- routes/products.js
+++ routes/products.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var router = express.Router();
-let seacher_product_title_controller = require('../controladores/seacher_product_title_controller')
-let salesController = require('../controladores/sales_controller')
-let priceController = require('../controladores/price_controller')
-let visualizePoduct = require('../controladores/visualize_product_controller')
-
+const seacher_product_title_controller = require('../controladores/seacher_product_title_controller')
+const salesController = require('../controladores/sales_controller');
+const priceController = require('../controladores/price_controller');
+const visualizePoduct = require('../controladores/visualize_product_controller')
+const orderController = require('../controladores/order_controller');
 
 router.get('/productos/:nombre', seacher_product_title_controller);
 router.get('/ventas', salesController);
 router.get('/precio', priceController)
 router.get('/visualizarproductos', visualizePoduct)
+
+router.post('/pedidos', orderController)
 
 
 module.exports = router;
