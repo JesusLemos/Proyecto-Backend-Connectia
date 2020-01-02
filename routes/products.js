780beb9082738a84806860b833a2517e443548b1
Laura Cruz <lcc4413@gmail.com>
2020-01-02 18:11:48 +0100

    topProvincia terminado

--- routes/products.js
+++ routes/products.js
@@ -1,20 +1,20 @@
 var express = require('express');
 var router = express.Router();
-const seacher_product_title_controller = require('../controladores/seacher_product_title_controller')
+const seacher_product_title_controller = require('../controladores/seacher_product_title_controller');
 const salesController = require('../controladores/sales_controller');
 const priceController = require('../controladores/price_controller');
-const visualizePoduct = require('../controladores/visualize_product_controller')
+const visualizePoduct = require('../controladores/visualize_product_controller');
 const orderController = require('../controladores/order_controller');
-const seacher_product_id_controller = require('../controladores/seacher_product_id_controller')
-
+const seacher_product_id_controller = require('../controladores/seacher_product_id_controller');
+const topProvinciasController = require('../controladores/topProvincias_controller');
 
 router.get('/productos/:nombre', seacher_product_title_controller);
 router.get('/ventas', salesController);
-router.get('/precio', priceController)
-router.get('/visualizarproductos', visualizePoduct)
-router.get('/mostrarproducto/:id', seacher_product_id_controller )
-
-router.post('/pedidos', orderController)
+router.get('/precio', priceController);
+router.get('/visualizarproductos', visualizePoduct);
+router.get('/mostrarproducto/:id', seacher_product_id_controller);
+router.get('/provinciastop', topProvinciasController);
+router.post('/pedidos', orderController);
 
 
 module.exports = router;
